45f2af6997e98931eca161643aae447477e6344a
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2017-09-08 21:41:36 +0200

    bootstarp added, icon, navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -10,6 +11,14 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { QuoteComponent } from './components/quote/quote.component';
 import { AddQuoteComponent } from './components/add-quote/add-quote.component';
 import { EditQuoteComponent } from './components/edit-quote/edit-quote.component';
+
+const appRoutes: Routes = [
+ {path:'',component:HomeComponent},
+ {path:'quotes-list',component:QuotesListComponent},
+ {path:'quote/:id', component:QuoteComponent},
+ {path:'add-quote',component:AddQuoteComponent},
+ {path:'edit-quote/:id', component:EditQuoteComponent}
+]
 
 @NgModule({
   declarations: [
@@ -24,7 +33,8 @@ import { EditQuoteComponent } from './components/edit-quote/edit-quote.component
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
