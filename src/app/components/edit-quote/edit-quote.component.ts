b854228ed95717d8c0c142b3874e53c9fd472fe3
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2017-10-23 22:10:48 +0200

    implemented edit quote

--- src/app/components/edit-quote/edit-quote.component.ts
+++ src/app/components/edit-quote/edit-quote.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../../services/firebase.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-edit-quote',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-quote.component.css']
 })
 export class EditQuoteComponent implements OnInit {
+  id;
+  title;
+  category;
+  imageUrl;
+  createdAt;
 
-  constructor() { }
+  constructor(
+    private firebaseService: FirebaseService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+    this.firebaseService.getQuoteDetails(this.id).subscribe(listing => {
+      this.title = listing.title;
+      this.category = listing.category;
+      this.imageUrl = listing.imageUrl;
+      this.createdAt = listing.createdAt;
+      console.log(this.title);
+    });
+  }
+
+  onEditSubmit() {
+    let listing = {
+      title: this.title,
+      category: this.category,
+      imageUrl: this.imageUrl,
+      createdAt: Date.now()
+    }
+    this.firebaseService.updateQuote(this.id, listing);
+    this.router.navigate(['/quotes-list']);
   }
 
 }
